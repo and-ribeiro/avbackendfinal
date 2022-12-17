eec82c0825b60ba3ea5f45e76bdfcaf828d44809
Anderson Ribeiro <andersonsiilvar@gmail.com>
2022-12-16 23:08:15 -0300

    primeiro

---
+++ backendfinal2/ms-users/src/main/java/com/viviansanches/msusers/model/Bill.java
@@ -0,0 +1,5 @@
+package com.viviansanches.msusers.model;
+
+public record Bill (String idBill, String customerBill, String productBill, Double totalPrice) {
+
+}
---
+++ backendfinal2/ms-users/src/test/java/com/viviansanches/msusers/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viviansanches.msusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
